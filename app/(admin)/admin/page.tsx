553c9f4684d79be32d13351d6a0f29b2f3596dec
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2024-04-17 08:51:47 +0500

    changed client & admin initial UI

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -8,6 +8,7 @@ import {
   Package,
   Package2,
   Search,
+  Settings,
   ShoppingCart,
   Users,
 } from "lucide-react";
@@ -85,23 +86,14 @@ const Page: React.FC = () => {
                 <LineChart className="h-4 w-4" />
                 Analytics
               </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Settings className="h-4 w-4" />
+                Settings
+              </Link>
             </nav>
-          </div>
-          <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
-              <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Button size="sm" className="w-full">
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card>
           </div>
         </div>
       </div>
