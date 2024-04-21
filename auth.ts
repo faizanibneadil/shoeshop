afa16f55c13e8a37dcf7d0a839f6e25904da1ff1
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2024-04-21 20:54:19 +0500

    supabase connected

--- auth.ts
+++ auth.ts
@@ -6,7 +6,7 @@ import { prisma } from "./config/db"
 
 export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
     callbacks: {
-        session: async ({ newSession, session, token, user, trigger }) => {
+        session: async ({ session, token, user, trigger }) => {
             session.user.id = token?.sub as string
             console.log("SESSION", session)
             return session
@@ -15,9 +15,6 @@ export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
             console.log("TOKEN", token)
             return token
         },
-        signIn: async ({ account, user, credentials, email, profile }) => {
-            return true
-        }
     },
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
